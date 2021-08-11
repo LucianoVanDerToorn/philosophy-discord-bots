89eb226578e5247671bd7b05ee934d9ea57d4c76
Luciano Nooijen <luciano@bytecode.nl>
2021-08-11 12:22:17 +0200

    Add first basic commands
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -20,7 +21,6 @@ func init() {
 }
 
 func main() {
-
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
@@ -31,9 +31,6 @@ func main() {
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
-	// TODO: Add MR to library to create db.UserUpdateStatusAndActivity:wq
-	//dg.UserUpdateStatus(discordgo.StatusOnline)
-
 	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
@@ -41,6 +38,23 @@ func main() {
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Set status and activity
+	err = dg.UpdateStatusComplex(discordgo.UpdateStatusData{
+		IdleSince: nil,
+		Activities: []*discordgo.Activity{
+			{
+				Name: "debating plebs at the agora",
+				Type: 5,
+			},
+		},
+		AFK:    false,
+		Status: "online",
+	})
+	if err != nil {
+		fmt.Println("error setting status,", err)
 		return
 	}
 
@@ -57,15 +71,103 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	fmt.Println(m.Content)
-
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "!socrates" {
-		s.ChannelMessageSend(m.ChannelID, "I'm the one who's asking questions here!")
+
+	// Only listen to messages starting with the correct prefix
+	const botPrefix = "!socrates"
+	contents := m.Content
+	if !strings.HasPrefix(contents, botPrefix) {
+		fmt.Printf("message '%s' does not have prefix %s\n", contents, botPrefix)
+		return
 	}
+	fmt.Printf("Found a message starting with '%s': '%s'\n", botPrefix, contents)
+
+	// Get the command given and run the correct handler
+	botCommand := func()string { // TODO: support arguments
+		parts := strings.Split(m.Content, " ")
+		if len(parts) <= 1 {
+			return ""
+		}
+		return parts[1]
+	}()
+	fmt.Printf("detected bot command '%s'\n", botCommand)
+	switch botCommand {
+	case "groupinfo":
+		botCommandGroupinfo(s, m)
+	case "help":
+		botCommandHelp(s, m)
+	default:
+		botCommandHelp(s, m)
+	}
+}
+
+func botCommandHelp(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "I'm Socrates, a hand-crafted bot for Bookclub Philosophy. You can ask me anything you like, but I only respond to `!socrates help` and `!socrates groupinfo` at this moment.")
+}
+
+func reportErrorMessage (s *discordgo.Session, channelId string, err error) {
+	const oopsieWhoopsieCopyPasta = "OOPSIE WOOPSIE!! Uwu We make a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!"
+	fmt.Printf("something went wrong: %s", err)
+	_, sendErr := s.ChannelMessageSend(channelId, fmt.Sprintf("%s\n(err: %s)", oopsieWhoopsieCopyPasta, err))
+	if sendErr != nil {
+		fmt.Printf("error sending error message to Discord: %s", err)
+	}
+}
+
+type ChannelData struct {
+	Name string
+	Leader string
+	MeetingEvery string
+	MeetingDay string
+	MeetingTimeGmt string
+	ReadingWhat string
+	DropboxLink string
+}
+func (cd ChannelData) EmbedDescription() string {
+	template := `*Info about the %s reading group*
+We meet every %s on **%s** at **%s** GMT
+We are currently reading **%s**
+The reading group leader is %s for any further questions`
+	return fmt.Sprintf(template, cd.Name, cd.MeetingEvery, cd.MeetingDay, cd.MeetingTimeGmt, cd.ReadingWhat, cd.Leader)
+}
+
+
+func botCommandGroupinfo(s *discordgo.Session, m *discordgo.MessageCreate) {
+	c, err := s.Channel(m.ChannelID)
+	if err != nil {
+		reportErrorMessage(s, m.ChannelID, err)
+	}
+	channel := c.Name
+	channelDataLookup := map[string]ChannelData {
+		"bot-commands": {
+			Name:           "Bot Commands",
+			Leader:         "Luciano",
+			MeetingEvery:   "week",
+			MeetingDay:     "Monday",
+			MeetingTimeGmt: "10AM",
+			ReadingWhat:    "Golang code",
+			DropboxLink:    "https://lucianonooijen.com",
+		},
+	}
+	cd, ok := channelDataLookup[channel]
+	if !ok {
+		fmt.Printf("channel data not found for channel %s", channel)
+		s.ChannelMessageSend(m.ChannelID, "I could not find the meeting data for this channel")
+		return
+	}
+
+	s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
+		Content:         "Here you go!",
+		Embed:           &discordgo.MessageEmbed{
+			Type:        discordgo.EmbedTypeRich,
+			Title:       fmt.Sprintf("Reading group info for the %s channel", channel),
+			Description: cd.EmbedDescription(),
+			URL: cd.DropboxLink,
+
+		},
+	})
 }
