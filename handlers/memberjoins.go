67776c55445f8498d2f530e5723364532d9679e9
Luciano Nooijen <luciano@bytecode.nl>
2021-10-06 23:19:20 +0300

    Update welcome message
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- handlers/memberjoins.go
+++ handlers/memberjoins.go
@@ -2,26 +2,29 @@ package handlers
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-const introductionsChannelId = "784549937345593415"
+const introductionsChannelId = "895401321514016830"
 const introductionMessageTemplate = `Welcome, %s! My name is Socrates, the hand-crafted bot for this Philosophy Bookclub server, nice to meet you.
 
-I would like to ask you a few questions. After that, a human moderator will give you access to the server, then you can pick your roles in the #roles channel.
+I would like to ask you a few questions. After that, a human moderator will give you access to the server, then you can pick your roles in the <#784552536044339201> channel.
 
-Feel free to omit the answers to questions you’re uncomfortable with. That said, providing this information will be helpful to us.
+An overview of all reading groups for the upcoming week can be found in <#883007166859083796>.
 
-1. Name, pronouns, age
-2. How did you find us?
-3. Education level and area of study (state whether you’re an autodidact, undergrad, graduate and what your major is/was):
-4. Specialization (ex: epistemology, philosophy of mind, phenomenology, ethics), if any:
-5. Briefly, why are you here?:
-6. Which philosophers have you read?
-7. Are you committed to the principle of charity? (See Rule 2 listed in #welcome)`
+1. What are your name, pronouns and age?
+2. How did you get interested in philosophy?
+3. What areas of  philosophy and which thinkers interest you?
+4. What kinds of people do you want to have discussions with?
+5. Do you agree with the <#784544880591896599> and in particular, the principle of charity?
+
+You can answer these questions in <#784549937345593415>`
 
 func MemberJoins(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
+	time.Sleep(2 * time.Second) // Wait two seconds to grasp the user's attention
+
 	mention := m.Mention()
 	message := fmt.Sprintf(introductionMessageTemplate, mention)
 
