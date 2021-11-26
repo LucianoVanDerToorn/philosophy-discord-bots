54717ce9187bef0565346f2816b01abbab02cf65
Luciano Nooijen <luciano@bytecode.nl>
2021-11-26 22:55:20 +0200

    Update Nietzsche meeting time
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- jobs/notify.go
+++ jobs/notify.go
@@ -18,7 +18,7 @@ func AddNotificationCron(s *discordgo.Session) {
 	c := cron.New()
 	addNotificationCronWeekly(c, s, "30 19 * * 3", "874324504401301564", "stirner", "874508087598383154")
 	addNotificationCronWeekly(c, s, "0 19 * * 4", "835711337979838464", "epistemology", "850752244793868298")
-	addNotificationCronWeekly(c, s, "0 13 * * 5", "827469681689755688", "nietzsche", "827528609081458718")
+	addNotificationCronWeekly(c, s, "0 14 * * 5", "827469681689755688", "nietzsche", "827528609081458718")
 	addNotificationCronWeekly(c, s, "30 18 * * 5", "903398560689700894", "marxism", "858963568955490335")
 	addNotificationCronWeekly(c, s, "0 23 * * 5", "896873495340916736", "taoism", "896875467632693248")
 	addNotificationCronWeekly(c, s, "0 13 * * 6", "784555854892367902", "plato", "784565469214146581")
