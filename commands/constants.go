da8f719376718471ec289a3a1d878ef2af062a97
Luciano van der Toorn <luciano@lucianovandertoorn.com>
2022-11-18 22:41:26 +0200

    Add some comments
    Signed-off-by: Luciano van der Toorn <luciano@lucianovandertoorn.com>

--- commands/constants.go
+++ commands/constants.go
@@ -1,10 +1,12 @@
 package commands
 
 const (
+	// Base commands for public use
 	commandIdSocrates = "socrates"
 	commandIdDiogenes = "diogenes"
 	commandIdFinegold = "finegold"
 
+	// Subcommands shared
 	commandSpeak  = "speak"
 	commandSource = "source"
 )
