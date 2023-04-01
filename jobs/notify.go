827f3bcd8998b07e6f239a003d22aa8c6d276a2d
Luciano van der Toorn <luciano@lucianovandertoorn.com>
2023-04-01 21:56:52 +0300

    Disable Nietzsche ping
    Signed-off-by: Luciano van der Toorn <luciano@lucianovandertoorn.com>

--- jobs/notify.go
+++ jobs/notify.go
@@ -18,7 +18,7 @@ func AddNotificationCron(s *discordgo.Session) {
 	c := cron.New()
 	addNotificationCronWeekly(c, s, "30 19 * * 1", "784556092814393394", "arendt", "784565934584889405")
 	addNotificationCronWeekly(c, s, "0 18 * * 3", "874324504401301564", "stirner", "874508087598383154")
-	addNotificationCronWeekly(c, s, "0 14 * * 5", "827469681689755688", "nietzsche", "827528609081458718")
+	//addNotificationCronWeekly(c, s, "0 14 * * 5", "827469681689755688", "nietzsche", "827528609081458718")
 	//addNotificationCronWeekly(c, s, "0 15 * * 0", "859164046922481705", "stoicism", "858964070749175808")
 	c.Start()
 }
